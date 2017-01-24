d096bdc8e9ef346ba6f1a4e61e25ad5fc9bae939
Philip Huysmans <philip.huysmans@gmail.com>
2017-01-24 19:34:36 +0100

    first deploy

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-var osmosis = require('osmosis');
+var express = require('express')
+var app = express()
 
-var stocks = [
-  "http://www.tijd.be/beurzen/AB_InBev.60128205",
-  "http://www.tijd.be/beurzen/Bekaert.60115401",
-  "http://www.tijd.be/beurzen/Engie.360194010",
-  "http://www.tijd.be/beurzen/Colruyt.60115403"
-];
+var scraper = require('./scraper');
 
-Promise.all(stocks.map(retrieve))
-  .then(function(data) {
-    console.log(data);
-  });
-
-function retrieve(url) {
-  return new Promise(function(resolve,reject){
-    var stockData = {};
-    osmosis
-      .get(url)
-      .find("div.l-main-container-article__article.js-stocks-detail.js-responsive-ads > h1")
-      .then(function(data) {
-        stockData["name"] = data.text().trim().split('                ')[0];
-      })
-      .find("span.koersenfiche-main-percentage.js-stock-detail-main-percentage")
-      .then(function(data) {
-        stockData["percentage"] = data.text().trim();
-      })
-      .find("span.koersenfiche-main-price.js-stock-detail-main-price")
-      .then(function(data) {
-        stockData["price"] = data.text().split(' ')[1];
-      })
-      .find("body > div.main-nav-table.js-main-nav-table > div.main-nav-column-content > div.l-main-container-article > div.l-grid--full > div.l-main-container-article-container > div.l-main-container-article__article.js-stocks-detail.js-responsive-ads > div:nth-child(2) > div.l-markten-live > div.l-main-container__section > div:nth-child(4) > div > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > span")
-      .then(function(data) {
-        console.log("DATA");
-        console.log(data);
-        stockData["table"] = data.text();
-      })
-      .done(function() {
-        resolve(stockData);
-      })
-      .log(console.log)
-      .error(console.log)
-      .debug(console.log)
+app.get('/', function (req, res) {
+  scraper()
+    .then(function(data) {
+      res.json(data);
     });
-}
+});
 
-/*http://www.tijd.be/ajax/historyPeriod?issueId=60115403*/
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port', app.get('port'));
+});
---
+++ scraper/index.js
@@ -0,0 +1,66 @@
+var osmosis = require('osmosis');
+
+var stocks = [
+  "AB_InBev.60128205",
+  "Bekaert.60115401",
+  "Engie.360194010",
+  "Colruyt.60115403"
+];
+
+function scrape() {
+  return Promise.all(stocks.map(retrieve));
+}
+
+function retrieve(url) {
+  return new Promise(function(resolve,reject){
+    var stockData = {};
+    
+    osmosis
+      .get(constructUrl(url))
+      .find("div.l-main-container-article__article.js-stocks-detail.js-responsive-ads > h1")
+      .then(function(data) {
+        stockData["name"] = data.text().trim().split('                ')[0];
+      })
+      .find("span.koersenfiche-main-percentage.js-stock-detail-main-percentage")
+      .then(function(data) {
+        stockData["percentage"] = data.text().trim();
+      })
+      .find("span.koersenfiche-main-price.js-stock-detail-main-price")
+      .then(function(data) {
+        stockData["price"] = data.text().split(' ')[1];
+      })
+      .get("http://www.tijd.be/ajax/historyPeriod?issueId=60115403")
+      .find("table")
+      .then(function(data) {
+        var table = data.text().split("    ");
+        table.splice(0,1);
+        var rows = table
+          .map(function(row) {
+            var rowParts = row.split(" ");
+            return {
+              timespan: rowParts[0] + " " + rowParts[1],
+              percent: rowParts[2],
+              high: rowParts[3],
+              low: rowParts[4]
+            }
+          });
+
+        stockData["history"] = rows;
+      })
+      .done(function() {
+        resolve(stockData);
+      });
+      /*
+      .log(console.log)
+      .error(console.log)
+      .debug(console.log)
+      */
+    });
+}
+
+function constructUrl(stock) {
+  return "http://www.tijd.be/beurzen/" + stock;
+}
+
+
+module.exports = scrape;
