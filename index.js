88bd0c86b347fb9d88ffed470c5957132184d52e
Philip Huysmans <philip.huysmans@gmail.com>
2017-03-03 23:29:59 +0100

    forgot to commit

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ var express = require('express');
 var app = express();
 
 var scraper = require('./scraper');
+var counter = 0;
+
+var checkIfOk = function(something) {
+  return something.reduce(function(allHistory, stock) {
+    return allHistory && stock.history.length > 0;
+  }, true);
+}
 
 var recoveringScraper = function() {
-    return scaper()
+  console.log("Executing", counter++);
+    return scraper()
       .then(function(data) {
-        if(data) {
+        if(checkIfOk(data)) {
           return data;
         } else {
           return recoveringScraper();
@@ -18,6 +26,13 @@ var recoveringScraper = function() {
 
 app.get('/', function (req, res) {
   scraper()
+    .then(function(data) {
+      res.json(data);
+    });
+});
+
+app.get('/withRecover', function(req,res) {
+  recoveringScraper()
     .then(function(data) {
       res.json(data);
     });
